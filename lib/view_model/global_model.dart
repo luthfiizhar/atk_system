13ea4addca1b0a7cf5ed2c158b0f33637104a1d9
luthfiizhar <59906471+luthfiizhar@users.noreply.github.com>
2023-05-02 17:32:26 +0700

    model and view model update

--- lib/view_model/global_model.dart
+++ lib/view_model/global_model.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class GlobalModel extends ChangeNotifier {
+  String? _businessUnit;
   String? _role;
   String? _areaId;
   String? _year;
   String? _month;
 
-  String get role => _role ?? "RegionalManager";
-  String get areaId => _areaId ?? "RM1";
+  String get businessUnit => _businessUnit ?? "1";
+  String get role => _role ?? "AreaManager";
+  String get areaId => _areaId ?? "AM 01";
   String get year => _year ?? DateTime.now().year.toString();
-  String get month => _month ?? DateFormat("MMM").format(DateTime.now());
+  String get month => _month ?? "Apr";
 
   void setRole(String value) {
     _role = value;
