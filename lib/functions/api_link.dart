148edc01cc987bca99ed33a9aab3fd7a119d6ea3
luthfiizhar <59906471+luthfiizhar@users.noreply.github.com>
2023-12-20 13:56:09 +0700

    Update api_link.dart

--- lib/functions/api_link.dart
+++ lib/functions/api_link.dart
@@ -2,5 +2,8 @@ class ApiConstant {
   // String apiUrl = 'fmklg.klgsys.com'; // Development
   String apiUrl = 'fmklg-backend.klgsys.com'; // Production
 
+  // String app = 'GSS_Backend'; //PRODUCTION APP
+  String app = 'EliteGSS_Backend'; //PRODUCTION APP ELITE
+
   // String get apiUrl => apiUrlGlobal;
 }
